d47b78691d483128f041eb0d4bc75ad7ff925cd9
Murillo Fabri <murilloeduardo123@gmail.com>
2021-01-08 16:04:49 -0300

    Update NC.py

--- NC.py
+++ NC.py
@@ -2,19 +2,27 @@ import pyautogui
 import time
 import libr
 import os
-
-file='C:\Program Files\pw3270\pw3270.exe'
-
-rg=str(input('Digite o RG: '))
-senha=str(input("Digite a Senha: "))
-libr.init(rg, senha)
+station=[]
+rg=[]
+senha=[]
+os='a'
 while True:
-    station=str(input('Digite a estação: '))
-    seq=str(input('Digite a sequência: '))
-    libr.nc(station, seq, rg)
-
-
-
-
+    rg = str(input('Digite o RG: '))
+    ims=str(input('Abrir IMS? (S/N) '))
+    if ims=='s' or ims=='S':
+        senha=str(input("Digite a Senha: "))
+        libr.init(rg, senha)
+    func=str(input('Qual função você quer executar?\n Apontamentos(A)\n Não Conformidades(NC)\n'))
+    if func=='NC' or func=='nc':
+            station=str(input('Digite a estação: '))
+            #seq=str(input('Digite a sequência: '))
+            while not os.isspace() and station!='esc' and station!='ESC':
+                libr.nc(station, rg)
+    #elif func=='a' or func=='A':
+        #False
+        #while True:
+            #station = str(input('Digite a estação: '))
+            #seq = str(input('Digite a sequência: '))
+            #Empregados separados por espaço:
 
 
